198539b3e30577e64e985785168e157f247573a6
Natnipa S <s.natnipa@gmail.com>
2024-04-07 12:06:57 +0200

    style: fix code style issues

--- frontend/src/routes/Header.svelte
+++ frontend/src/routes/Header.svelte
@@ -2,10 +2,10 @@
 	import logo from '$lib/images/logo.svg';
 
 	let pages = [
-    { text: 'How It Works', url: '/about' },
-    { text: 'Learning Models', url: '/sverdle' },
-    { text: 'My Profile', url: '#' },
-  ];
+		{ text: 'How It Works', url: '/about' },
+		{ text: 'Learning Models', url: '/sverdle' },
+		{ text: 'My Profile', url: '#' }
+	];
 </script>
 
 <header>
@@ -13,27 +13,27 @@
 	<nav class="bg-gray-200 p-4 flex items-center justify-between">
 		<!-- Logo -->
 		<div class="flex items-center">
-		  <!-- SVG Logo -->
-		  <a href="/" class="flex items-center">
-			<img src={logo} alt="Davincin" class="h-8 w-auto ml-4 mr-2 mb-2" />
-		  </a>
-		  <!-- Logo Text -->
-		  <span class="text-blue-600 text-sm mt-2">da Vinci to nth power</span>
+			<!-- SVG Logo -->
+			<a href="/" class="flex items-center">
+				<img src={logo} alt="Davincin" class="h-8 w-auto ml-4 mr-2 mb-2" />
+			</a>
+			<!-- Logo Text -->
+			<span class="text-blue-600 text-sm mt-2">da Vinci to nth power</span>
 		</div>
-		
+
 		<!-- Pages -->
 		<div class="flex items-center space-x-12 ml-auto font-semibold">
 			{#each pages as page}
-				 <a href={page.url} class="text-black hover:text-gray-300">{page.text}</a>
+				<a href={page.url} class="text-black hover:text-gray-300">{page.text}</a>
 			{/each}
 		</div>
-		
+
 		<!-- Buttons -->
 		<div class="flex items-center space-x-4 ml-10 font-semibold">
-		  <button class="text-blue-600 px-4 py-2 hover:text-blue-500">Login</button>
-		  <button class="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-500">Sign Up</button>
+			<button class="text-blue-600 px-4 py-2 hover:text-blue-500">Login</button>
+			<button class="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-500">Sign Up</button>
 		</div>
-	  </nav>
+	</nav>
 </header>
 
 <style>
