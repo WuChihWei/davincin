8496b3feec9bb21a8dfd7b227c038d63868e00ba
TreeMan888 <qwe30138@gmail.com>
2024-04-07 16:43:30 +0800

    feat: MVC structure

--- backend/app.js
+++ backend/app.js
@@ -1,67 +1,21 @@
-const fs = require('fs');
 const mysql = require("mysql");
-
 const express = require('express');
+
 const app = express();
 const hostname = '127.0.0.1'; // Your server ip address
 const port = 3000;
+
 // main page
 app.get('/', (req, res) => {
     res.send(`Server is up!`);
 })
 
 // Include routes file
-const usersRoute = require('./routes/collections');
-app.use('/collections', usersRoute);
+const collectionsRoute = require('./routes/collections');
+app.use('/collections', collectionsRoute);
+const profilesRoute = require('./routes/profiles');
+app.use('/profiles', profilesRoute);
 
-
-// Get Collections
-app.get('/collections', (req, res)=> {
-    // read file
-    fs.readFile('./Collections.json', 'utf-8', (err, data)=> {
-        if (err) {
-            return res.status(500).send("Sorry, something went wrong!");
-        }
-        const collections = JSON.parse(data);
-        return res.json({collections: collections})
-    })
-})
-
-app.get('/collections/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10)-1;
-    fs.readFile('./Collections.json', 'utf-8', (err, data)=> {
-        if (err) {
-            return res.status(500).send("Sorry, something went wrong!");
-        }
-        const collections = JSON.parse(data);
-        return res.json({collections: collections[id]})
-    })
-        
-    })
-
-// Get Profile
-app.get('/profiles', (req, res)=> {
-    // read file
-    fs.readFile('./Profiles.json', 'utf-8', (err, data)=> {
-        if (err) {
-            return res.status(500).send("Sorry, something went wrong!");
-        }
-        const profiles = JSON.parse(data);
-        return res.json({profiles: profiles})
-    })
-})
-
-app.get('/profiles/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10)-1;
-    fs.readFile('./Profiles.json', 'utf-8', (err, data)=> {
-        if (err) {
-            return res.status(500).send("Sorry, something went wrong!");
-        }
-        const profiles = JSON.parse(data);
-        return res.json({profiles: profiles[id]})
-    })
-        
-    })
 
     /*
 // Connect MYSQL
@@ -82,8 +36,6 @@ connection.connect(function(err) {
 app.get('/health', (req, res) => {
     res.status(200).send(`Health Check...`);
 })
-
-
 
 app.listen(port, () => {
     console.log(`Server running...`);
--- backend/routes/profiles.js
+++ backend/routes/profiles.js
@@ -1,31 +1,29 @@
-// routes/collections.js
+// routes/profiles.js
 const express = require('express');
 const fs = require('fs');
 
 const router = express.Router();
-
-// http://localhost:3000/collections
+// Get Profile
 router.get('/', (req, res)=> {
     // read file
-    fs.readFile('./Collections.json', 'utf-8', (err, data) => {
+    fs.readFile('./Profiles.json', 'utf-8', (err, data)=> {
         if (err) {
             return res.status(500).send("Sorry, something went wrong!");
         }
-        const collections = JSON.parse(data);
-        return res.json({collections: collections});
+        const profiles = JSON.parse(data);
+        return res.json({profiles: profiles})
     })
 })
 
-// http://localhost:3000/collections/:id
 router.get('/:id', (req, res) => {
     const id = parseInt(req.params.id, 10)-1;
-    fs.readFile('./Collections.json', 'utf-8', (err, data)=> {
+    fs.readFile('./Profiles.json', 'utf-8', (err, data)=> {
         if (err) {
             return res.status(500).send("Sorry, something went wrong!");
         }
-        const collections = JSON.parse(data);
-        return res.json({collections: collections[id]})
-    })
+        const profiles = JSON.parse(data);
+        return res.json({profiles: profiles[id]})
+    })        
 })
 
 // export the router module so that app.js file can use it
