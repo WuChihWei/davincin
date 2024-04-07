624a558a0901975eb61e640dbebe7a3643fbe7d4
Natnipa S <s.natnipa@gmail.com>
2024-04-07 16:45:14 +0200

    refactor(CollectionCard): rename component

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import CollectionTile from './CollectionTile.svelte';
+	import CollectionCard from './CollectionCard.svelte';
 	import image from '$lib/images/davinci_1.png';
 
 	const collections = [
@@ -64,7 +64,7 @@
 	<div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
 		<div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
 			{#each collections as collection}
-				<CollectionTile {collection} />
+				<CollectionCard {collection} />
 			{/each}
 		</div>
 	</div>
