9e159e173b9870897513daa967915c79740a2cbc
Natnipa S <s.natnipa@gmail.com>
2024-04-29 02:14:16 +0200

    fix: fix UntrustedHost error

--- src/auth.ts
+++ src/auth.ts
@@ -2,8 +2,11 @@ import prisma from '$lib/prisma';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Google from '@auth/sveltekit/providers/google';
+import { AUTH_SECRET } from "$env/static/private" 
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
 	adapter: PrismaAdapter(prisma),
-	providers: [Google]
+	providers: [Google],
+	trustHost: true,
+  	secret: AUTH_SECRET,
 });
