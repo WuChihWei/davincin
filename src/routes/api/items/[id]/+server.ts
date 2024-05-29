ea48659948c0f7f089998ae300ad46bc36a0b8bf
Natnipa S <s.natnipa@gmail.com>
2024-05-29 14:16:33 +0200

    feat(api): improve vote api

--- src/routes/api/items/[id]/+server.ts
+++ src/routes/api/items/[id]/+server.ts
@@ -1,22 +1,40 @@
 import { json } from '@sveltejs/kit';
 import prisma from '$lib/prisma';
+import { VoteType } from '@prisma/client';
 
 // PUT /api/items/:id
 export async function PUT({ request, params }) {
-	const { profileId, title, mediaType, description, vote } = await request.json();
+	// TODO: get userId from auth header
+	const { profileId, title, mediaType, description, vote, userId } = await request.json();
 	let item;
-	if (vote == 'up') {
+	if (vote == VoteType.UP) {
 		item = await prisma.mediaItem.update({
 			where: { id: params.id },
 			data: {
 				voteCount: { increment: 1 }
 			}
 		});
-	} else if (vote == 'down') {
+
+		await prisma.vote.create({
+			data: {
+				type: VoteType.UP,
+				userId: userId,
+				itemId: params.id
+			}
+		});
+	} else if (vote == VoteType.DOWN) {
 		item = await prisma.mediaItem.update({
 			where: { id: params.id },
 			data: {
 				voteCount: { decrement: 1 }
+			}
+		});
+
+		await prisma.vote.create({
+			data: {
+				type: VoteType.UP,
+				userId: userId,
+				itemId: params.id
 			}
 		});
 	} else {
