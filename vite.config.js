26866864c5c4e0cc67f08988ce405822223bfdfe
Natnipa S <s.natnipa@gmail.com>
2024-05-29 17:33:26 +0200

    fix: resolve @prisma/client/index-browser error

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,29 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { createRequire } from 'module';
+import path from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "davincin",
-            project: "davincin-app"
-        }
-    }), sveltekit()]
-});
+const { resolve } = createRequire(import.meta.url);
+
+const prismaClient = `prisma${path.sep}client`;
+
+const prismaClientIndexBrowser = resolve('@prisma/client/index-browser').replace(
+	`@${prismaClient}`,
+	`.${prismaClient}`
+);
+
+export default defineConfig(() => ({
+	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: 'davincin',
+				project: 'davincin-app'
+			}
+		}),
+		sveltekit()
+	],
+	resolve: {
+		alias: { '.prisma/client/index-browser': path.relative(__dirname, prismaClientIndexBrowser) }
+	}
+}));
