d0ab1957c8d01a29551e6150157178d6a8f0e14a
Natnipa S <s.natnipa@gmail.com>
2024-05-29 17:32:26 +0200

    feat: set up sentry to handle error log

---
+++ src/hooks.client.js
@@ -0,0 +1,21 @@
+import { handleErrorWithSentry, replayIntegration } from '@sentry/sveltekit';
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+	dsn: 'https://053c38bef4b1256df903d08462b97111@o4507340149030912.ingest.de.sentry.io/4507340151390288',
+	tracesSampleRate: 1.0,
+
+	// This sets the sample rate to be 10%. You may want this to be 100% while
+	// in development and sample at a lower rate in production
+	replaysSessionSampleRate: 0.1,
+
+	// If the entire session is not sampled, use the below sample rate to sample
+	// sessions when an error occurs.
+	replaysOnErrorSampleRate: 1.0,
+
+	// If you don't want to use Session Replay, just remove the line below:
+	integrations: [replayIntegration()]
+});
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,12 @@
+import * as Sentry from '@sentry/sveltekit';
 import { redirect, type Handle } from '@sveltejs/kit';
 import { handle as authenticationHandle } from './auth';
 import { sequence } from '@sveltejs/kit/hooks';
+
+Sentry.init({
+	dsn: 'https://053c38bef4b1256df903d08462b97111@o4507340149030912.ingest.de.sentry.io/4507340151390288',
+	tracesSampleRate: 1
+});
 
 /** @type {import('@sveltejs/kit').Handle} */
 async function authorizationHandle({ event, resolve }) {
@@ -18,4 +24,10 @@ async function authorizationHandle({ event, resolve }) {
 // First handle authentication, then authorization
 // Each function acts as a middleware, receiving the request handle
 // And returning a handle which gets passed to the next function
-export const handle: Handle = sequence(authenticationHandle, authorizationHandle);
+export const handle: Handle = sequence(
+	Sentry.sentryHandle(),
+	sequence(authenticationHandle, authorizationHandle)
+);
+
+/** @type {import('@sveltejs/kit').HandleServerError} */
+export const handleError = Sentry.handleErrorWithSentry();
